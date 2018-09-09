bdb33c4a0550a1cc8cfde0297b54004f9fbf4613
Chris de Graaf <chrisadegraaf@gmail.com>
2018-09-09 12:09:35 -0500

    Version v0.1.0

--- mix.exs
+++ mix.exs
@@ -7,15 +7,28 @@ defmodule OsuReplayParser.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      name: "osu! Replay Parser",
+      description: "A parser for osu replays (.osr files).",
+      source_url: "https://github.com/christopher-dG/osu-replay-parser-ex",
+      homepage_url: "https://github.com/christopher-dG/osu-replay-parser-ex"
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub": "https://github.com/christopher-dG/osu-replay-parser-ex",
+        "File Format Documentation": "https://osu.ppy.sh/help/wiki/osu!_File_Formats/Osr_(file_format"
+      }
     ]
   end
 end
