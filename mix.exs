aab76aaf2c2883404cfa9022ec21ba58810be85d
Chris de Graaf <chrisadegraaf@gmail.com>
2018-09-09 13:21:31 -0500

    Version 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OsuReplayParser.MixProject do
   def project do
     [
       app: :osu_replay_parser,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
